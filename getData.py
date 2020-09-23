2a1d4598240d2dad41ec22e02940577694665244
JacksonBurns <JacksonBurns@users.noreply.github.com>
2020-09-23 14:23:06 -0400

    add data retrieval logic

--- getData.py
+++ getData.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
 
 
-def ParseXML(filename):
+def getData(filename):
     """
     One line passthrough to xml element tree, returns root of tree
 
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from checkFor import checkFor
+from getData import getData
 
-def main():
-    data = open(r'name.csv')
-    for conversation in data:
-        result = checkFor(data,r'regex')
-    print("test")
-
+def main(num_conv=10, filename='merged-pythondev-help.xml'):
+    # read data in from xml
+    data = getData(filename)
+    # group data by conversation id
+    conversations = {}
+    for message in data[4:]:  # ignore starting info
+        conv_id = int(message.items()[0][1])  # conversation number
+        if(conv_id not in conversations):
+            conversations[conv_id] = [message[2].text]
+        else:
+            conversations[conv_id] = conversations[conv_id] + [message[2].text]
+    for i in range(1,num_conv+1):
+        messages = conversations[i]
+        result = checkFor(messages,r'Ok')
+        print(result)
 
 
 if __name__ == "__main__":
-    main()
+    main(num_conv=50)
