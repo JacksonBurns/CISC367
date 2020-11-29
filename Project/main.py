58032e218da048caa1fed47185fd79ac94acc757
JacksonBurns <JacksonBurns@users.noreply.github.com>
2020-11-29 17:09:04 -0500

    add option to retrieve results without running

--- Project/main.py
+++ Project/main.py
@@ -1,11 +1,20 @@
 # retrieve the other methods
 from funcs import checkFor, openXML, uprint
 from test import testMain
-import time, csv
+import time, csv, json
 
-def main():
-    print("Starting automated message retrieval")
-    print("")
+def main(pullFromJSON=True):
+    if(pullFromJSON):
+        print("Loading results from JSON")
+        results = json.load(open('automated_results.dict'))
+    else:
+        print("Starting automated message retrieval")
+        print("")
+        results = getQuestions()
+
+    print("Starting comparison to Gold Set")
+
+def getQuestions():
     results = {}
     print("Analyzing Slack data sets")
     for data in ['2017_Slack_Data.xml','2018_Slack_Data.xml','2019_Slack_Data.xml']:
@@ -27,7 +36,6 @@ def main():
         print("""~~~ RESULTS ~~~\n# Messages: {}\n# w/ Unpuncuated Questions: {}\nExecution Time: {:.2f} (s)"""
             .format(totalMessages-1,len(questions),time.time()-start))
         results[data] = questions
-    
     print("")
     print("Analyzing Kaggle data set")
     # reset timer
@@ -46,9 +54,10 @@ def main():
     print("""~~~ RESULTS ~~~\n# Messages: {}\n# w/ Unpuncuated Questions: {}\nExecution Time: {:.2f} (s)"""
         .format(totalMessages-1,len(questions),time.time()-start))
     results['KaggleData.csv'] = questions
-    print("")
-    print("Starting comparison to Gold Set")
-    
 
+    json.dump(results, open("automated_results.dict",'w'))
+    print("")
+    return results
+    
 if __name__ == "__main__":
-    main()
+    main(pullFromJSON=True)
