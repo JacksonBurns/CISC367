9cf5f634340a4dabb15c075c25414f1cdc6c10e7
JacksonBurns <JacksonBurns@users.noreply.github.com>
2020-11-29 16:02:00 -0500

    fix off by one and add new data

--- Project/main.py
+++ Project/main.py
@@ -5,23 +5,30 @@ import time, csv
 
 def main():
     print("Starting automated message retrieval")
-    print("Analyzing Slack data set")
-    # start a timer for this analysis
-    start = time.time()
-    # retrieve the data
-    allData = openXML.getData("data/merged-pythondev-help.xml")
-    # truncate irrelevant data
-    allData = allData[4:]
-    # iterate through each message in the data set
-    totalMessages = 0
-    questions = []
-    for message in allData:
-        if(checkFor.isQuestion(message[2].text,checkUnpuncuated=True)):
-            questions.append(totalMessages)
-        totalMessages += 1
-    # dispaly the results
-    print("""~~~ RESULTS ~~~\n# Messages: {}\n# w/ Unpuncuated Questions: {}\nExecution Time: {:.2f} (s)"""
-        .format(totalMessages,len(questions),time.time()-start))
+    print("")
+    results = {}
+    print("Analyzing Slack data sets")
+    for data in ['2017_Slack_Data.xml','2018_Slack_Data.xml','2019_Slack_Data.xml']:
+        print("Filename: " + data)
+        # start a timer for this analysis
+        start = time.time()
+        # retrieve the data
+        allData = openXML.getData('data/'+data)
+        # truncate irrelevant data
+        allData = allData[4:]
+        # iterate through each message in the data set
+        totalMessages = 1
+        questions = []
+        for message in allData:
+            if(checkFor.isQuestion(message[2].text,checkUnpuncuated=True)):
+                questions.append(totalMessages)
+            totalMessages += 1
+        # dispaly the results
+        print("""~~~ RESULTS ~~~\n# Messages: {}\n# w/ Unpuncuated Questions: {}\nExecution Time: {:.2f} (s)"""
+            .format(totalMessages-1,len(questions),time.time()-start))
+        results[data] = questions
+    
+    print("")
     print("Analyzing Kaggle data set")
     # reset timer
     start = time.time()
@@ -29,7 +36,7 @@ def main():
     with open('data/KaggleData.csv','r',errors='ignore') as file:
         reader = csv.reader(file)
         # iterate through each message in the data set
-        totalMessages = 0
+        totalMessages = 1
         questions = []
         for row in reader:
             if(checkFor.isQuestion(row[1],checkUnpuncuated=True)):
@@ -37,9 +44,9 @@ def main():
             totalMessages += 1
     # dispaly the results
     print("""~~~ RESULTS ~~~\n# Messages: {}\n# w/ Unpuncuated Questions: {}\nExecution Time: {:.2f} (s)"""
-        .format(totalMessages,len(questions),time.time()-start))
-
-    
+        .format(totalMessages-1,len(questions),time.time()-start))
+    results['KaggleData.csv'] = questions
+    print("")
     print("Starting comparison to Gold Set")
     
 
